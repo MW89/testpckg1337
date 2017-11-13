c2329627ccad78e43d94e4f3044bb907e3162b66
Max Westphal <mwestphal@uni-bremen.de>
2017-11-13 12:06:40 +0100

    split functions hello/hello.name

--- R/hello.R
+++ R/hello.R
@@ -12,25 +12,3 @@ hello <- function() {
   return("Hello, world!")
 }
 
-#' Say hello to anybody
-#'
-#' This function returns the string "Hello, *name*!" where name is the input of
-#' type character that the user needs to input.
-#'
-#' @param name A character specifying who you want to greet.
-#'
-#' @return Character.
-#'
-#' @note An error is thrown if the input name is not a character.
-#'
-#' @examples
-#' hello.name("Ann")
-#' x <- hello.name("Christian")
-#' x
-#'
-#' @export
-hello.name <- function(name){
-  if(class(name)!="character")stop("Only character objects are accepted as input.")
-  paste0("Hello, ", name, "!")
-}
-
