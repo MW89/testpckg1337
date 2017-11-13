1a9eddb26cb8cc6f9c283039e79d5908c557beae
Max Westphal <mwestphal@uni-bremen.de>
2017-11-13 12:14:00 +0100

    merged all tests to tests.R

--- tests/hello_test.R
+++
@@ -1,2 +0,0 @@
-library(testthat)
-expect_equal(hello(), "Hello, world!")
--- tests/tests.R
+++ tests/tests.R
@@ -1,5 +1,10 @@
 library(testthat)
+library(testpckg1337)
+
+# test for hello()
+expect_equal(hello(), "Hello, world!")
+
+# tests for hello.name()
 expect_equal(hello.name("Ann"), "Hello, Ann!")
 expect_equal(hello.name("Christian"), "Hello, Christian!")
 expect_error(hello.name(1337))
-
